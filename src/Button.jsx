795a72fc975d7b060991e9fb6413c610ed3b0bee
Alessandro Di Benedetto <socialorne02@gmail.com>
2025-04-01 09:45:40 +0200

    complete

---
+++ src/Button.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function Button({ text }) {
+  return <button>{text}</button>;
+}
+
+export default Button;
---
+++ src/Immagine.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function Button({ src, alt }) {
+  return <img src={src} alt={alt} />;
+}
+
+export default Button;
